1323b01934aff00853582a212caa00d9fd614b5f
Based Jamali <basedjamali@gmail.com>
2024-05-29 10:17:29 +0200

    Update Kalorienbedarf.java

--- Kalorienbedarf.java
+++ Kalorienbedarf.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Kalorienbedarf {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Willkommensgrüße
+        // Willkommensgruesse
         System.out.println("=== Willkommen bei HealthyEats, dem besten Kalorienbedarfsrechner! ===");
 
         // Eingabe des Namens
@@ -11,7 +11,7 @@ public class Kalorienbedarf {
         String name = scanner.nextLine();
 
         System.out.println("\nHallo " + name
-                + ", mit unserem Programm können Sie berechnen, wie viel Kalorien Sie täglich brauchen, um fit zu bleiben.\n");
+                + ", mit unserem Programm koennen Sie berechnen, wie viel Kalorien Sie taeglich brauchen, um fit zu bleiben.\n");
         System.out.println("Starten wir nun mit der Berechnung!");
 
         // Geschlecht
@@ -40,13 +40,13 @@ public class Kalorienbedarf {
             }
         }
 
-        // Größe
-        double größe = 0;
+        // Groesse
+        double groesse = 0;
         while (true) {
-            System.out.println("3. Wie groß sind Sie [in cm]:");
-            größe = scanner.nextDouble();
+            System.out.println("3. Wie gross sind Sie [in cm]:");
+            groesse = scanner.nextDouble();
 
-            if (größe <= 0) {
+            if (groesse <= 0) {
                 System.out.println("Die Eingabe ist falsch. Versuchen Sie es erneut!");
             } else {
                 break;
@@ -66,28 +66,28 @@ public class Kalorienbedarf {
             }
         }
 
-        // Aktivitätslevel
-        double aktivitätsfaktor = 0;
+        // Aktivitaetslevel
+        double aktivitaetsfaktor = 0;
         while (true) {
-            System.out.println("5. Wählen Sie Ihr Aktivitätslevel:");
+            System.out.println("5. Waehlen Sie Ihr Aktivitaetslevel:");
             System.out.println("   [1. Kaum oder keine Bewegung]");
             System.out.println("   [2. Leichte Bewegung (Sport 1-3 Tage/Woche)]");
-            System.out.println("   [3. Mäßige Bewegung (Sport 3-5 Tage/Woche)]");
+            System.out.println("   [3. Maessige Bewegung (Sport 3-5 Tage/Woche)]");
             System.out.println("   [4. Starke Bewegung (Sport 6-7 Tage/Woche)]");
-            System.out.println("   [5. Sehr starke Bewegung (hartes Training, körperlich anstrengender Beruf)]");
-            int aktivität = scanner.nextInt();
+            System.out.println("   [5. Sehr starke Bewegung (hartes Training, koerperlich anstrengender Beruf)]");
+            int aktivitaet = scanner.nextInt();
 
-            if (aktivität >= 1 && aktivität <= 5) {
-                if (aktivität == 1) {
-                    aktivitätsfaktor = 1.2;
-                } else if (aktivität == 2) {
-                    aktivitätsfaktor = 1.375;
-                } else if (aktivität == 3) {
-                    aktivitätsfaktor = 1.55;
-                } else if (aktivität == 4) {
-                    aktivitätsfaktor = 1.725;
-                } else if (aktivität == 5) {
-                    aktivitätsfaktor = 1.9;
+            if (aktivitaet >= 1 && aktivitaet <= 5) {
+                if (aktivitaet == 1) {
+                    aktivitaetsfaktor = 1.2;
+                } else if (aktivitaet == 2) {
+                    aktivitaetsfaktor = 1.375;
+                } else if (aktivitaet == 3) {
+                    aktivitaetsfaktor = 1.55;
+                } else if (aktivitaet == 4) {
+                    aktivitaetsfaktor = 1.725;
+                } else if (aktivitaet == 5) {
+                    aktivitaetsfaktor = 1.9;
                 }
                 break;
             } else {
@@ -97,13 +97,13 @@ public class Kalorienbedarf {
 
         // Harris-Bennedict-Formel
         if (geschlecht.equals("m")) {
-            double kalorienbedarfMann = 66.47 + ((13.7 * gewicht) + (5 * größe) - (6.8 * alter)) * aktivitätsfaktor;
-            System.out.println("\nIhr täglicher Kalorienbedarf beträgt: " + kalorienbedarfMann + " kcal\n");
+            double kalorienbedarfMann = 66.47 + ((13.7 * gewicht) + (5 * groesse) - (6.8 * alter)) * aktivitaetsfaktor;
+            System.out.println("\nIhr taeglicher Kalorienbedarf betraegt: " + kalorienbedarfMann + " kcal\n");
         } else if (geschlecht.equals("w")) {
-            double kalorienbedarfFrau = 655.1 + ((9.6 * gewicht) + (1.8 * größe) - (4.7 * alter)) * aktivitätsfaktor;
-            System.out.println("\nIhr täglicher Kalorienbedarf beträgt: " + kalorienbedarfFrau + " kcal\n");
+            double kalorienbedarfFrau = 655.1 + ((9.6 * gewicht) + (1.8 * groesse) - (4.7 * alter)) * aktivitaetsfaktor;
+            System.out.println("\nIhr taeglicher Kalorienbedarf betraegt: " + kalorienbedarfFrau + " kcal\n");
         }
 
-        System.out.println("Danke für Ihre Anmeldung und bleiben Sie gesund!");
+        System.out.println("Danke fuer Ihre Anmeldung und bleiben Sie gesund!");
     }
 }
